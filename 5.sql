b4ddbbb89bbb30c46f2a3f4504aed841e2d3f986
riedhampattel <riedhampattel.tops@gmail.com>
2025-01-25 16:28:06 +0530

    practice

--- 5.sql
+++ 5.sql
@@ -5,3 +5,9 @@ create user Rohan identified by 'abc@123' default role faculty;
 grant select,insert on 11_january.employees to Rohan; 
 
 revoke insert on 11_january.employees from Rohan;
+
+grant select on 11_january.demo to Rohan;
+
+grant insert on 11_january.demo to Rohan;
+
+select * from demo;
